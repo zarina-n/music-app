a3cb6a8039adecd25564b07742b06a52f4148aaa
Zarina <zarina_nugmanova@mail.ru>
2022-09-16 13:33:07 -0400

    deleted extra import and tags

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AppRoutes from "./pages/AppRoutes";
 import { ThemeProvider } from "styled-components";
 import { dark, light } from "./styles/Theme.styled";
 import GlobalStyles from "./styles/GlobalStyles";
-import { SkeletonTheme } from "react-loading-skeleton";
 
 export const ThemeContext = React.createContext("theme");
 
@@ -18,14 +17,12 @@ function App() {
   );
 
   return (
-    <SkeletonTheme>
-      <ThemeContext.Provider value={themeValue}>
-        <ThemeProvider theme={darkTheme ? dark : light}>
-          <GlobalStyles />
-          <AppRoutes user={user} />
-        </ThemeProvider>
-      </ThemeContext.Provider>
-    </SkeletonTheme>
+    <ThemeContext.Provider value={themeValue}>
+      <ThemeProvider theme={darkTheme ? dark : light}>
+        <GlobalStyles />
+        <AppRoutes user={user} />
+      </ThemeProvider>
+    </ThemeContext.Provider>
   );
 }
 
